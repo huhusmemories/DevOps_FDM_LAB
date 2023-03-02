268793503197c3b74e81eda1706dc7d3e1edc95a
huang.shewei <huangshewei@gmail.com>
2023-03-02 12:52:56 -0500

    Created resources with interpolation, count, and for each. Variables are in a separated file

--- main.tf
+++ main.tf
@@ -12,18 +12,20 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "b" {
-  bucket = "my-bucket-aws-hs"
-}
-
-resource "aws_s3_bucket" "bucket" {
-
-}
-
+# Create aws instance
 resource "aws_instance" "inst" {
-
+  ami           = var.ami_value
+  instance_type = var.ins_type_t3_micro
 }
 
-resource "aws_instance" "inst-2" {
+# Create buckets with interpolation
+resource "aws_s3_bucket" "bucket" {
+  bucket = "${var.bucket-name}-${count.index}"
+  count  = var.count-number
+}
 
+# Create users
+resource "aws_iam_user" "users" {
+  for_each = toset(var.list-users)
+  name     = each.value
 }
